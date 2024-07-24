07fa30df5d7ba3b793d703c145ffa0dc430414ed
Udemy98 <jankodedic3@gmail.com>
2024-07-25 00:01:48 +0200

    Two way binding

--- Client/Pages/Dismissible.razor.cs
+++ Client/Pages/Dismissible.razor.cs
@@ -4,8 +4,22 @@ namespace SecondBlazorProject.Client.Pages
 {
     public partial class Dismissible
     {
+        private bool show;
         [Parameter]
-        public bool Show { get; set; }
+        public bool Show
+        {
+            get => show;
+            set
+            {
+                if (value != show)
+                {
+                    show = value;
+                    ShowChanged?.Invoke(show);
+                }
+            }
+        }
+        [Parameter]
+        public Action<bool>? ShowChanged { get; set; }
         [Parameter]
         public RenderFragment ChildContent { get; set; } = default!;
         public void Dismiss()
--- Client/obj/Debug/net6.0/SecondBlazorProject.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net6.0/SecondBlazorProject.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecondBlazorProject.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16c73a27dd0c9a8c4bf882479b913b92aff3f309")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecondBlazorProject.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecondBlazorProject.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
